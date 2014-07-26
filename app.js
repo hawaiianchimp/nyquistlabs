c5dd6fe056a953c10566c09f908b38085e91ec9b
Sean Thomas Burke <hawaiianchimp@gmail.com>
2014-07-26 01:15:54 +0000

    fixing url

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var http = require('http');
 var path = require('path');
 var exphbs  = require('express3-handlebars');
 var mongoose = require('mongoose');
-var mongo_url = (typeof MONGOHQ_URL != 'undefined') ? MONGOHQ_URL:'mongodb://localhost/db';
+var mongo_url = (typeof process.env.MONGOHQ_URL != 'undefined') ? process.env.MONGOHQ_URL:'mongodb://localhost/db';
 mongoose.connect(mongo_url);
 
 
